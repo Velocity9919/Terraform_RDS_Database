f2c18b4d8964c7335353080cc629a2e6d561d8f8
Velocity9919 <143435067+Velocity9919@users.noreply.github.com>
2023-12-02 12:15:14 +0530

    Update main.tf

---
+++ MariaDB/main.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+    region     = "${var.region}"
+    access_key = "${var.access_key}"
+    secret_key = "${var.secret_key}"
+}
+
+
+#create a RDS Database Instance
+resource "aws_db_instance" "myrds" {
+  engine               = "mariadb"
+  identifier           = "myrds"
+  allocated_storage    =  20
+  engine_version       = "10.6.14"
+  instance_class       = "db.t2.micro"
+  username             = "admin"
+  password             = "admin123"
+  skip_final_snapshot  = true
+  publicly_accessible =  true
+}
